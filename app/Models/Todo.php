a5c8cf3b7ec8a57342dc15c63b75e76091e39f66
S4nfs <zyzzaestheticsgods@gmail.com>
2022-05-06 14:38:56 +0530

    mass assignment property added

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'completed'];
+    protected $fillable = ['title', 'completed', 'user_id'];
 }
 
 
