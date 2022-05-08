6427be725fd35a1ed399dc89c18064950a905d4d
S4nfs <zyzzaestheticsgods@gmail.com>
2022-05-08 12:40:20 +0530

    alert component

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
-</div>
+{{-- php artisan make:component Alert --}}
+@if (session()->has('message'))
+    <div class="py-4 px-2 bg-green-300" role="alert">
+        {{ session()->get('message') }}
+    </div>
+@endif
+
+@if ($errors->any())
+    <div class="py-4 px-2 bg-red-300" role="alert">
+        <ul>
+            @foreach ($errors->all() as $erroritem)
+                <li>{{ $erroritem }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
