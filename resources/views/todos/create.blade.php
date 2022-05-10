4ca3c9ed6845dc96c47e99fa277c6b00e381de32
S4nfs <zyzzaestheticsgods@gmail.com>
2022-05-10 18:29:01 +0530

    description and steps

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -2,15 +2,30 @@
 @section('content')
     <div class="text-center py-10 flex justify-center items-center">
         <div class="text-center py-20 ">
-        <h1 class="text-4xl">What next you need To-DO</h1>
-        <x-Alert />
-        <form action="/todos/create" method="post" class="py-5">
-            @csrf
-            <input type="text" name="title" class="py-3 px-2 border rounded">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-10 ml-5 rounded-full"
-                type="submit">Create</button>
-        </form>
-        <a href="/todos"><button class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-bold py-2 px-4 rounded-full mt-5" type="submit">Back</button></a>
+            <h1 class="text-4xl">What next you need To-DO</h1>
+            <x-Alert />
+            <form action="/todos/create" method="post" class="py-5">
+                @csrf
+                <div class="py-1">
+                    <input type="text" name="title" class="py-3 px-2 border rounded" placeholder="Title">
+                </div>
+                <div class="py-1">
+                    <textarea name="description" id="" class="p-2 rounded border" placeholder="Description"></textarea>
+                </div>
+                <div class="py-1">
+@livewire('todosteps')
+                    </div>
+                
+                <div class="py-1"><button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-10 mt-5 rounded-full"
+                        type="submit">Create</button>
+                </div>
+            </form>
+            <a href="/todos/"><button
+                    class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-bold py-1 px-5 rounded-full"
+                    type="submit">Back</button>
+            </a>
+
         </div>
     </div>
 @endsection
