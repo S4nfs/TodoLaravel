61eef5b2f6ac1c7444e376f649fa3d8647b8d555
S4nfs <zyzzaestheticsgods@gmail.com>
2022-05-12 18:31:32 +0530

    wire:key will tell Livewire how to keep track of a DOM element

--- resources/views/livewire/todosteps.blade.php
+++ resources/views/livewire/todosteps.blade.php
@@ -4,9 +4,10 @@
         <span wire:click="increment" class="bi bi-plus-circle px-2 py-2 cursor-pointer">
     </div>
     @foreach($steps as $list)
-    <div class="flex justify-center py-2">
-        <input type="text" name="steps[]" class="py-3 px-2 border rounded" placeholder="{{'Describe steps '.$list}}">
-        <span wire:click="decrement({{$loop->index}})" class="bi bi-x px-2 text-2xl" style="color:red;">
+    <div class="flex justify-center py-2" wire:key="{{$list}}">
+        <input type="text" name="steps[]" class="py-3 px-2 border rounded" placeholder="{{'step '.($list + 1)}}"> 
+        {{-- $loop is variable given by laravel only for foreach --}}
+        <span wire:click="decrement({{$list}})" class="bi bi-x px-2 text-2xl" style="color:red;">
 </div>
         @endforeach
 </div>
