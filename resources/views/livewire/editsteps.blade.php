f7fb0268023b5b277413aade75573fdc89135018
S4nfs <zyzzaestheticsgods@gmail.com>
2022-05-14 19:31:32 +0530

    livewire for update steps

---
+++ resources/views/livewire/editsteps.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <div class="flex justify-center pb-4 px-4">
+        <h2 class="text-2xl pb-4">Add Steps for tasks</h2>
+        <span wire:click="increment" class="bi bi-plus-circle px-2 py-2 cursor-pointer">
+    </div>
+    @foreach($steps as $list)
+    <div class="flex justify-center py-2" wire:key="{{$loop->index}}">
+        <input type="text" name="steps[]" class="py-3 px-2 border rounded" placeholder="{{'step '.($loop->index + 1)}}" value="{{$list['name']}}"> 
+        {{-- $loop is variable given by laravel only for foreach --}}
+        <span wire:click="decrement({{$loop->index}})" class="bi bi-x px-2 text-2xl" style="color:red;">
+</div>
+        @endforeach
+</div>
--- resources/views/todos/edit.blade.php
+++ resources/views/todos/edit.blade.php
@@ -15,6 +15,9 @@
                         placeholder="Description">{{ $id->description }}</textarea>
                 </div>
                 <div class="py-1">
+                    @livewire('editsteps', ['steps' => $id->steps])
+                </div>
+                <div class="py-1">
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-10 mt-5 rounded-full"
                         type="submit">Update</button>
                 </div>
