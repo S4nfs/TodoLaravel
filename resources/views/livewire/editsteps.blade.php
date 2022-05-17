17d9ff315a8dd2903f4dd12b0b9bc7913085c493
S4nfs <zyzzaestheticsgods@gmail.com>
2022-05-17 14:19:37 +0530

    grabbing steps name and id

--- resources/views/livewire/editsteps.blade.php
+++ resources/views/livewire/editsteps.blade.php
@@ -3,11 +3,12 @@
         <h2 class="text-2xl pb-4">Add Steps for tasks</h2>
         <span wire:click="increment" class="bi bi-plus-circle px-2 py-2 cursor-pointer">
     </div>
-    @foreach($steps as $list)
-    <div class="flex justify-center py-2" wire:key="{{$loop->index}}">
-        <input type="text" name="steps[]" class="py-3 px-2 border rounded" placeholder="{{'step '.($loop->index + 1)}}" value="{{$list['name']}}"> 
-        {{-- $loop is variable given by laravel only for foreach --}}
-        <span wire:click="decrement({{$loop->index}})" class="bi bi-x px-2 text-2xl" style="color:red;">
-</div>
-        @endforeach
+    @foreach ($steps as $list)
+        <div class="flex justify-center py-2" wire:key="{{ $loop->index }}">
+            <input type="text" name="stepsName[]" class="py-3 px-2 border rounded" placeholder="{{ 'step ' . ($loop->index+1) }}" value="{{ $list['name'] }}" />
+            <input type="hidden" name="stepsId[]" value="{{ $list['id'] }}" />
+            {{-- $loop is variable given by laravel only for foreach --}}
+            <span wire:click="decrement({{ $loop->index }})" class="bi bi-x px-2 text-2xl" style="color:red;">
+        </div>
+    @endforeach
 </div>
